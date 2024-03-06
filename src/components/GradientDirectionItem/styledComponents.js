10a36e51eaa3f97dd3543b8f4eaf50f60c25064b
chinnaragamsetti <ragamsettirajkumar@gamil.com>
2024-03-06 00:38:17 +0000

    6-3-24

--- src/components/GradientDirectionItem/styledComponents.js
+++ src/components/GradientDirectionItem/styledComponents.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
 export const CustomButton = styled.button`
-  background-color:#ffffff
+  background-color: #ffffff;
   opacity: ${props => (props.IsActive === true ? 1 : 0.5)};
-  color:${props => (props.IsActive ? '#334155' : '#1e293b')};
+  color: ${props => (props.IsActive ? '#334155' : '#1e293b')};
   border-radius: 3px;
   margin-right: 5px;
   padding-left: 10px;
@@ -11,7 +11,7 @@ export const CustomButton = styled.button`
   font-size: 10px;
   border-radius: 5px;
   outline: none;
-  border:none;
+  border: none;
   width: 50px;
-  cursor:pointer;
+  cursor: pointer;
 `
